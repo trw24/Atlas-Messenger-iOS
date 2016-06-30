dc86228bbaa5a0d0f4fa1415dc6bf5999d4764aa
Klemen Verdnik <klemen.verdnik@gmail.com>
2016-06-30 10:01:52 -0700

    ATLMApplicationController now UI free.

--- Code/Controllers/ATLMApplicationController.h
+++ Code/Controllers/ATLMApplicationController.h
@@ -20,20 +20,110 @@
 
 
 #import <Foundation/Foundation.h>
+#import <LayerKit/LYRClient.h>
 #import "ATLMAuthenticationProvider.h"
-#import "ATLMLayerClient.h"
 
-@class ATLMSplitViewController;
-
-extern NSString * _Nonnull const ATLMLayerApplicationID;
 extern NSString * _Nonnull const ATLMConversationMetadataDidChangeNotification;
 extern NSString * _Nonnull const ATLMConversationParticipantsDidChangeNotification;
 extern NSString * _Nonnull const ATLMConversationDeletedNotification;
 
+extern NSString *_Nonnull const ATLMApplicationControllerErrorDomain;
+
+typedef NS_ENUM(NSUInteger, ATLMApplicationControllerError) {
+    ATLMApplicationControllerErrorAppIDAlreadySet                  = 1, // Layer appID already set on the application controller.
+    ATLMApplicationControllerErrorFailedHandlingRemoteNotification = 2, // Underlying Layer client failed to handle the remote notification.
+};
+
+///-------------------------
+/// @name Application States
+///-------------------------
+
+typedef NS_ENUM(NSUInteger, ATLMApplicationState) {
+    /**
+     @abstract A state where the app doesn't have a Layer appID yet.
+     */
+    ATLMApplicationStateAppIDNotSet             = 1,
+    
+    /**
+     @abstract A state where the app has the appID, but no user credentials.
+     */
+    ATLMApplicationStateCredentialsRequired     = 2,
+    
+    /**
+     @abstract A state where the app is fully authenticated.
+     */
+    ATLMApplicationStateAuthenticated           = 3
+};
+
+@class ATLMApplicationController;
+
 /**
- @abstract The `ATLMApplicationController` manages global resources needed by multiple view controller classes in the Atlas Messenger App.
- It also implement the `LYRClientDelegate` protocol. Only one inst ance should be instantiated and it should be passed to 
- controllers that require it.
+ @abstract The `ATLMApplicationControllerDelegate` notifies the receiver about
+   the application state changes so that the receiver can navigate the UI accordingly.
+ */
+@protocol ATLMApplicationControllerDelegate <NSObject>
+
+@optional
+
+/**
+ @abstract Notifies the receiver the application state has changed.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ @param applicationState New application state.
+ */
+- (void)applicationController:(nonnull ATLMApplicationController *)applicationController didChangeState:(ATLMApplicationState)applicationState;
+
+/**
+ @abstract Notifies the receiver the application controller has finished handling
+   the remote notification and hands all the objects associated with the remote
+   notification.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ @param conversation The `LYRConversation` instance associated with the remote notification.
+ @param conversation The `LYRMessage` instance associated with the remote notification.
+ */
+- (void)applicationController:(nonnull ATLMApplicationController *)applicationController didFinishHandlingRemoteNotificationForConversation:(nullable LYRConversation *)conversation message:(nullable LYRMessage *)message;
+
+/**
+ @abstract Notifies the receiver that the underlying Layer Client will attempt to establish a connection.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ @param attemptNumber The number of attempts the underlying client has attempted to make.
+ @param delayInterval The amount of time until the underlying Layer Client will make the next attempt.
+ @param attemptLimit The maximum number of attempts the underlying Layer Client will make.
+ */
+- (void)applicationController:(nonnull ATLMApplicationController *)applicationController willAttemptToConnect:(NSUInteger)attemptNumber afterDelay:(NSTimeInterval)delayInterval maximumNumberOfAttempts:(NSUInteger)attemptLimit;
+
+/**
+ @abstract Notifies the receiver that the underlying Layer Client has successfully established a connection.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ */
+- (void)applicationControllerDidConnect:(nonnull ATLMApplicationController *)applicationController;
+
+/**
+ @abstract Notifies the receiver that the underlying Layer Client has disconnected.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ */
+- (void)applicationControllerDidDisconnect:(nonnull ATLMApplicationController *)applicationController;
+
+/**
+ @abstract Notifies the receiver that the underlying Layer Client has lost a connection.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ @param error The error associated with the connection loss.
+ */
+- (void)applicationController:(nonnull ATLMApplicationController *)applicationController didLoseConnectionWithError:(nonnull NSError *)error;
+
+/**
+ @abstract Notifies the receiver the application controller has hit an error.
+ @param applicationController The `ATLMApplicationController` instance performing the invocation.
+ @param error The error instance the application controller has hit.
+ */
+- (void)applicationController:(nonnull ATLMApplicationController *)applicationController didFailWithError:(nonnull NSError *)error;
+
+@end
+
+/**
+ @abstract The `ATLMApplicationController` manages global resources needed by
+   multiple view controller classes in the Atlas Messenger App. It also
+   implements the `LYRClientDelegate` protocol. Only one instance should be
+   instantiated and it should be passed to controllers that require it.
  */
 @interface ATLMApplicationController : NSObject <LYRClientDelegate>
 
@@ -42,32 +132,78 @@ extern NSString * _Nonnull const ATLMConversationDeletedNotification;
 ///--------------------------------
 
 /**
- @abstract Initializes the `ATLMApplicationController` instance with the supplied provider. 
- @param provider An object conforming to the `ATLMAuthenticating protocol. 
+ @abstract Creates the `ATLMApplicationController` instance with the supplied provider.
+ @param provider An object conforming to the `ATLMAuthenticating protocol.
+ @param layerClientOptions The Layer client's options instance, which will be passed
+   to the `LYRClient` during its initialization.
+ @return Returns an instance of the `ATLMApplicationController` ready for use.
+ @discussion The application controller creates an instance of the `LYRClient` once the
+   appID is known.
  */
-+ (nonnull instancetype)applicationControllerWithAuthenticationProvider:(nonnull id<ATLMAuthenticating>)authenticationProvider;
++ (nonnull instancetype)applicationControllerWithAuthenticationProvider:(nonnull id<ATLMAuthenticating>)authenticationProvider layerClientOptions:(nullable LYRClientOptions *)layerClientOptions;
+
+/**
+ @abstract Updates the application controller with the Layer appID. Updating the
+   appID will create and assign an instance of the `LYRClient` on this
+   `ATLMApplicationController` instance.
+ @param appID The Layer AppID used to initialize an underlying `LYRClient`
+   instance.
+ @param error An `out` error reference instance will be set in case the
+   operation failed.
+ @return Returns `YES` if the appID was set and the underlying `LYRClient` instance
+   was successfully created; In case the method is called when the appID has
+   already been set, the method will return `NO`.
+ @warning The appID can be set only once, other attempts will be ignored and the
+   method will return `NO` with an error.
+ */
+- (BOOL)setAppID:(nonnull NSURL *)appID error:(NSError *_Nullable *_Nullable)error;
 
 /**
  @abstract Authenticates the application by performing the Layer authentication handshake.
  @param credentials An `NSDictionary` containing authetication credentials. 
  @param completions A block to be called upon completion of the operation.
  */
-- (void)authenticateWithCredentials:(nonnull NSDictionary *)credentials completion:(nonnull void (^)(LYRSession * _Nonnull session, NSError * _Nonnull error))completion;
+- (void)authenticateWithCredentials:(nonnull NSDictionary *)credentials completion:(nonnull void (^)(LYRSession * _Nonnull session, NSError *_Nullable error))completion;
 
 /**
- @abstract Updates the controller with a `LYRClient` instance.
- @param client The `LYRClient` instancce.
+ @abstract Updates the remote notification device token on the underlying `LYRClient` insance.
+ @param deviceToken The remote notification device token passed by the app delegate
+   upon receiving a device token.
  */
-- (void)updateWithLayerClient:(nonnull LYRClient *)client;
+- (void)updateRemoteNotificationDeviceToken:(nullable NSData *)deviceToken;
 
-///--------------------------------
+/**
+ @abstract Passes the remote notification to the client to handle it, which
+   will cause a short synchronization process and call the completion handler
+   once the synchronization completes.
+ @param userInfo The remote notification dictionary passed by the app
+   delegate upon receiving a remote notification or user responding to it.
+ @param completionHandler A block to be called upon completion of the
+   synchronization process. The `completionHandler` will always be executed,
+   no matter if the underlying client handled the remote notification
+   successfully, hit an error, or if the remote notification was not meant
+   for the underlying `layerClient`.
+ */
+- (void)handleRemoteNotification:(nonnull NSDictionary *)userInfo completion:(nonnull void (^)(BOOL success, NSError *_Nullable error))completionHandler;
+
+///-----------------------
 /// @name Global Resources
-///--------------------------------
+///-----------------------
+
+/**
+ @abstract The receiver in charge of handling application state changes and errors.
+ */
+@property (nullable, nonatomic, weak) id<ATLMApplicationControllerDelegate> delegate;
 
 /**
  @abstract The `LSAPIManager` object for the application.
  */
-@property (nonnull, nonatomic, readonly) id <ATLMAuthenticating> authenticationProvider;
+@property (nonnull, nonatomic, readonly) id<ATLMAuthenticating> authenticationProvider;
+
+/**
+ @abstract The Layer appID used to initialize the underlying `LYRClient`.
+ */
+@property (nullable, nonatomic, readonly) NSURL *appID;
 
 /**
  @abstract The `LYRClient` object for the application.
@@ -75,8 +211,45 @@ extern NSString * _Nonnull const ATLMConversationDeletedNotification;
 @property (nullable, nonatomic, readonly) LYRClient *layerClient;
 
 /**
- @abstract The `ATLMSplitViewController` controller which is the application's root controller.
+ @abstract The state the application controller is currently in.
  */
-@property (nullable, weak, nonatomic) ATLMSplitViewController *splitViewController;
+@property (assign, nonatomic, readonly) ATLMApplicationState state;
+
+/**
+ @abstract Queries the underlying LayerKit client for the total count of `LYRMessage` objects whose `isUnread` property is true.
+ */
+@property (assign, nonatomic, readonly) NSUInteger countOfUnreadMessages;
+
+/**
+ @abstract Queries the underlying LayerKit client for the total count of `LYRMessage` objects.
+ */
+@property (assign, nonatomic, readonly) NSUInteger countOfMessages;
+
+/**
+ @abstract Queries the underlying LayerKit client for the total count of `LYRConversation` objects.
+ */
+@property (assign, nonatomic, readonly) NSUInteger countOfConversations;
+
+/**
+ @abstract Queries LayerKit for an existing message whose `identifier` property matches the supplied identifier.
+ @param identifier An NSURL representing the `identifier` property of an `LYRMessage` object for which the query will be performed.
+ @retrun An `LYRMessage` object or `nil` if none is found.
+ */
+- (nullable LYRMessage *)messageForIdentifier:(nonnull NSURL *)identifier;
+
+/**
+ @abstract Queries LayerKit for an existing conversation whose `identifier` property matches the supplied identifier.
+ @param identifier An NSURL representing the `identifier` property of an `LYRConversation` object for which the query will be performed.
+ @retrun An `LYRConversation` object or `nil` if none is found.
+ */
+- (nullable LYRConversation *)existingConversationForIdentifier:(nonnull NSURL *)identifier;
+
+/**
+ @abstract Queries LayerKit for an existing conversation whose `participants` property matches the supplied set.
+ @param participants An `NSSet` of participant identifier strings for which the query will be performed.
+ @retrun An `LYRConversation` object or `nil` if none is found.
+ */
+- (nullable LYRConversation *)existingConversationForParticipants:(nonnull NSSet *)participants;
+
 
 @end
--- Code/Controllers/ATLMApplicationController.m
+++ Code/Controllers/ATLMApplicationController.m
@@ -22,28 +22,43 @@
 #import <SVProgressHUD/SVProgressHUD.h>
 #import "ATLMErrors.h"
 #import "ATLMConstants.h"
+#import "ATLMessagingUtilities.h"
 
 NSString *const ATLMLayerApplicationID = @"LAYER_APP_ID";
 NSString *const ATLMConversationMetadataDidChangeNotification = @"LSConversationMetadataDidChangeNotification";
 NSString *const ATLMConversationParticipantsDidChangeNotification = @"LSConversationParticipantsDidChangeNotification";
 NSString *const ATLMConversationDeletedNotification = @"LSConversationDeletedNotification";
+NSString *const ATLMApplicationControllerErrorDomain = @"ATLMApplicationControllerErrorDomain";
 
 @interface ATLMApplicationController ()
+
+@property (nonnull, nonatomic, readwrite) id<ATLMAuthenticating> authenticationProvider;
+@property (nullable, nonatomic, readwrite) NSURL *appID;
+@property (nullable, nonatomic, readwrite) LYRClient *layerClient;
+@property (assign, nonatomic, readwrite) ATLMApplicationState state;
+@property (nonatomic, readwrite, copy) LYRClientOptions *layerClientOptions;
 
 @end
 
 @implementation ATLMApplicationController
 
-+ (instancetype)applicationControllerWithAuthenticationProvider:(id<ATLMAuthenticating>)authenticationProvider
++ (instancetype)applicationControllerWithAuthenticationProvider:(id<ATLMAuthenticating>)authenticationProvider layerClientOptions:(LYRClientOptions *)layerClientOptions
 {
-    return [[self alloc] initWithAuthenticationProvider:authenticationProvider];
+    return [[self alloc] initWithAuthenticationProvider:authenticationProvider layerClientOptions:layerClientOptions];
 }
 
-- (id)initWithAuthenticationProvider:(id<ATLMAuthenticating>)authenticationProvider
+- (id)initWithAuthenticationProvider:(id<ATLMAuthenticating>)authenticationProvider layerClientOptions:(LYRClientOptions *)layerClientOptions
 {
     self = [super init];
     if (self) {
         _authenticationProvider = authenticationProvider;
+        _layerClientOptions = layerClientOptions;
+        _state = ATLMApplicationStateAppIDNotSet;
+        NSString *appIDString = [[NSUserDefaults standardUserDefaults] valueForKey:ATLMLayerApplicationID];
+        NSURL *appID = [NSURL URLWithString:appIDString];
+        if (appID) {
+            [self setAppID:appID error:nil];
+        }
     }
     return self;
 }
@@ -53,53 +68,150 @@ NSString *const ATLMConversationDeletedNotification = @"LSConversationDeletedNot
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-#pragma mark Authentication
+- (void)setState:(ATLMApplicationState)state
+{
+    if (_state == state) {
+        // Prevent to notify the delegate with the same state.
+        return;
+    }
+    _state = state;
+    if ([self.delegate respondsToSelector:@selector(applicationController:didChangeState:)]) {
+        [self.delegate applicationController:self didChangeState:state];
+    }
+}
+
+#pragma mark - LayerKit client initialization and configuration
+
+- (BOOL)setAppID:(nonnull NSURL *)appID error:(NSError *__autoreleasing _Nullable*)error
+{
+    if (self.appID) {
+        // Prevent from re-setting the appID.
+        if (error) {
+            *error = [NSError errorWithDomain:ATLMApplicationControllerErrorDomain code:ATLMApplicationControllerErrorAppIDAlreadySet userInfo:@{ NSLocalizedDescriptionKey: @"Failed to set the appID because it has been previously set." }];
+        }
+        return NO;
+    }
+    _appID = appID;
+    
+    // Persist the appID into the user defaults.
+    [[NSUserDefaults standardUserDefaults] setValue:appID.absoluteString forKey:ATLMLayerApplicationID];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    // Associate the authentication model with the new appID.
+    [self.authenticationProvider updateWithAppID:appID];
+    
+    // Create a new instance of LYRClient and configure it.
+    [self initializeLayerClient];
+    
+    // Bump the application state.
+    if (self.layerClient.currentSession.state == LYRSessionStateAuthenticated) {
+        self.state = ATLMApplicationStateAuthenticated;
+    } else {
+        self.state = ATLMApplicationStateCredentialsRequired;
+    }
+    return YES;
+}
+
+- (void)initializeLayerClient
+{
+    self.layerClient = [LYRClient clientWithAppID:self.appID delegate:self options:self.layerClientOptions];
+    self.layerClient.autodownloadMIMETypes = [NSSet setWithObjects:ATLMIMETypeImageJPEGPreview, ATLMIMETypeTextPlain, nil];
+    
+    // Connect if possible.
+    if (!self.layerClient.isConnected && !self.layerClient.isConnecting) {
+        [self.layerClient connectWithCompletion:^(BOOL success, NSError *error) {
+            NSLog(@"Layer Client Connected");
+        }];
+    }
+}
 
 - (void)authenticateWithCredentials:(NSDictionary *)credentials completion:(void (^)(LYRSession *session, NSError *error))completion
 {
+    __weak typeof(self) weakSelf = self;
     [self.layerClient requestAuthenticationNonceWithCompletion:^(NSString * _Nullable nonce, NSError * _Nullable error) {
-        if (nonce) {
+        if (!nonce) {
             completion(nil, error);
+            return;
         }
-        [self.authenticationProvider authenticateWithCredentials:credentials nonce:nonce completion:^(NSString * _Nonnull identityToken, NSError * _Nonnull error) {
-            if (identityToken) {
+        [weakSelf.authenticationProvider authenticateWithCredentials:credentials nonce:nonce completion:^(NSString * _Nonnull identityToken, NSError * _Nonnull error) {
+            if (!identityToken) {
                 completion(nil, error);
+                return;
             }
-            [self.layerClient authenticateWithIdentityToken:identityToken completion:^(LYRIdentity * _Nullable authenticatedUser, NSError * _Nullable error) {
+            [weakSelf.layerClient authenticateWithIdentityToken:identityToken completion:^(LYRIdentity * _Nullable authenticatedUser, NSError * _Nullable error) {
                 if (authenticatedUser) {
-                    completion(nil, error);
+                    completion(weakSelf.layerClient.currentSession, nil);
+                    weakSelf.state = ATLMApplicationStateAuthenticated;
                 } else {
-                    completion(self.layerClient.currentSession, nil);
+                    completion(nil, error);
                 }
             }];
         }];
     }];
 }
 
-#pragma mark Update Layer Client
-
-- (void)updateWithLayerClient:(nonnull LYRClient *)client
+- (void)updateRemoteNotificationDeviceToken:(NSData *)deviceToken
 {
-    _layerClient = client;
+    NSError *error;
+    BOOL success = [self.layerClient updateRemoteNotificationDeviceToken:deviceToken error:&error];
+    if (!success) {
+        [self notifyDelegateOfError:error];
+    }
 }
 
-#pragma mark - LYRClientDelegate
+#pragma mark - Remote notification handling
+
+- (void)handleRemoteNotification:(NSDictionary *)userInfo completion:(void (^)(BOOL success, NSError *_Nullable error))completionHandler
+{
+    BOOL success = [self.layerClient synchronizeWithRemoteNotification:userInfo completion:^(LYRConversation * _Nullable conversation, LYRMessage * _Nullable message, NSError * _Nullable error) {
+        if (conversation || message) {
+            completionHandler(YES, nil);
+        } else {
+            if (error) {
+                // In case the client hit an error during the synchronization process.
+                completionHandler(NO, error);
+            } else {
+                // In case the client didn't receive any conversations or messages.
+                completionHandler(YES, nil);
+            }
+        }
+    }];
+    if (!success) {
+        completionHandler(YES, nil);
+    }
+}
+
+#pragma mark - LYRClientDelegate implementation
 
 - (void)layerClient:(LYRClient *)client didReceiveAuthenticationChallengeWithNonce:(NSString *)nonce
 {
+    NSLog(@"Layer Client did receive an authentication challenge with nonce=%@", nonce);
     [self.authenticationProvider refreshAuthenticationWithNonce:nonce completion:^(NSString * _Nonnull identityToken, NSError * _Nonnull error) {
-        //
+        if (!identityToken) {
+            [self notifyDelegateOfError:error];
+            return;
+        }
+        // Pass the new identity token to the client to reestablish the session.
+        [self.layerClient authenticateWithIdentityToken:identityToken completion:^(LYRIdentity * _Nullable authenticatedUser, NSError * _Nullable error) {
+            if (!authenticatedUser) {
+                [self notifyDelegateOfError:error];
+            }
+        }];
     }];
 }
 
 - (void)layerClient:(LYRClient *)client didAuthenticateAsUserID:(NSString *)userID
 {
-    NSLog(@"Layer Client did recieve authentication nonce");
+    NSLog(@"Layer Client did authenticate as userID=%@", userID);
+    // Bumping the application state to "authenticated".
+    self.state = ATLMApplicationStateAuthenticated;
 }
 
 - (void)layerClientDidDeauthenticate:(LYRClient *)client
 {
     NSLog(@"Layer Client did deauthenticate");
+    // Revert the state back to "AppIDCollected".
+    self.state = ATLMApplicationStateCredentialsRequired;
 }
 
 - (void)layerClient:(LYRClient *)client objectsDidChange:(NSArray *)changes
@@ -108,15 +220,12 @@ NSString *const ATLMConversationDeletedNotification = @"LSConversationDeletedNot
         if (![change.object isKindOfClass:[LYRConversation class]]) {
             continue;
         }
-        
         if (change.type == LYRObjectChangeTypeUpdate && [change.property isEqualToString:@"metadata"]) {
             [[NSNotificationCenter defaultCenter] postNotificationName:ATLMConversationMetadataDidChangeNotification object:change.object];
         }
-        
         if (change.type == LYRObjectChangeTypeUpdate && [change.property isEqualToString:@"participants"]) {
             [[NSNotificationCenter defaultCenter] postNotificationName:ATLMConversationParticipantsDidChangeNotification object:change.object];
         }
-        
         if (change.type == LYRObjectChangeTypeDelete) {
             [[NSNotificationCenter defaultCenter] postNotificationName:ATLMConversationDeletedNotification object:change.object];
         }
@@ -126,30 +235,95 @@ NSString *const ATLMConversationDeletedNotification = @"LSConversationDeletedNot
 - (void)layerClient:(LYRClient *)client didFailOperationWithError:(NSError *)error
 {
     NSLog(@"Layer Client did fail operation with error: %@", error);
+    [self notifyDelegateOfError:error];
 }
 
 - (void)layerClient:(LYRClient *)client willAttemptToConnect:(NSUInteger)attemptNumber afterDelay:(NSTimeInterval)delayInterval maximumNumberOfAttempts:(NSUInteger)attemptLimit
 {
-    if (attemptNumber == 1) {
-        [SVProgressHUD showWithStatus:@"Connecting to Layer"];
-    } else {
-        [SVProgressHUD showWithStatus:[NSString stringWithFormat:@"Connecting to Layer in %lus (%lu of %lu)", (unsigned long)ceil(delayInterval), (unsigned long)attemptNumber, (unsigned long)attemptLimit]];
+    NSLog(@"Layer Client will attempt to connect after %.2fs; attempt=%lu; maximumAttempts=%lu", delayInterval, attemptNumber, attemptLimit);
+    if ([self.delegate respondsToSelector:@selector(applicationController:willAttemptToConnect:afterDelay:maximumNumberOfAttempts:)]) {
+        [self.delegate applicationController:self willAttemptToConnect:attemptNumber afterDelay:delayInterval maximumNumberOfAttempts:attemptLimit];
     }
 }
 
 - (void)layerClientDidConnect:(LYRClient *)client
 {
-    [SVProgressHUD showSuccessWithStatus:@"Connected to Layer"];
-}
-
-- (void)layerClient:(LYRClient *)client didLoseConnectionWithError:(NSError *)error
-{
-    [SVProgressHUD showErrorWithStatus:[NSString stringWithFormat:@"Lost Connection: %@", error.localizedDescription]];
+    NSLog(@"Layer Client did connect");
+    if ([self.delegate respondsToSelector:@selector(applicationControllerDidConnect:)]) {
+        [self.delegate applicationControllerDidConnect:self];
+    }
 }
 
 - (void)layerClientDidDisconnect:(LYRClient *)client
 {
-    [SVProgressHUD showSuccessWithStatus:@"Disconnected from Layer"];
+    NSLog(@"Layer Client did disconnect");
+    if ([self.delegate respondsToSelector:@selector(applicationControllerDidConnect:)]) {
+        [self.delegate applicationControllerDidDisconnect:self];
+    }
+}
+
+- (void)layerClient:(LYRClient *)client didLoseConnectionWithError:(NSError *)error
+{
+    NSLog(@"Layer Client did lose connection");
+    if ([self.delegate respondsToSelector:@selector(applicationControllerDidConnect:)]) {
+        [self.delegate applicationController:self didLoseConnectionWithError:error];
+    }
+}
+
+#pragma mark - Helpers
+
+- (void)notifyDelegateOfError:(NSError *)error
+{
+    if (error) {
+        if ([self.delegate respondsToSelector:@selector(applicationController:didFailWithError:)]) {
+            [self.delegate applicationController:self didFailWithError:error];
+        }
+    }
+}
+
+- (NSUInteger)countOfUnreadMessages
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRMessage class]];
+    LYRPredicate *unreadPred =[LYRPredicate predicateWithProperty:@"isUnread" predicateOperator:LYRPredicateOperatorIsEqualTo value:@(YES)];
+    LYRPredicate *userPred = [LYRPredicate predicateWithProperty:@"sender.userID" predicateOperator:LYRPredicateOperatorIsNotEqualTo value:self.layerClient.authenticatedUser.userID];
+    query.predicate = [LYRCompoundPredicate compoundPredicateWithType:LYRCompoundPredicateTypeAnd subpredicates:@[unreadPred, userPred]];
+    return [self.layerClient countForQuery:query error:nil];
+}
+
+- (NSUInteger)countOfMessages
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRMessage class]];
+    return [self.layerClient countForQuery:query error:nil];
+}
+
+- (NSUInteger)countOfConversations
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRConversation class]];
+    return [self.layerClient countForQuery:query error:nil];
+}
+
+- (LYRMessage *)messageForIdentifier:(NSURL *)identifier
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRMessage class]];
+    query.predicate = [LYRPredicate predicateWithProperty:@"identifier" predicateOperator:LYRPredicateOperatorIsEqualTo value:identifier];
+    query.limit = 1;
+    return [self.layerClient executeQuery:query error:nil].firstObject;
+}
+
+- (LYRConversation *)existingConversationForIdentifier:(NSURL *)identifier
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRConversation class]];
+    query.predicate = [LYRPredicate predicateWithProperty:@"identifier" predicateOperator:LYRPredicateOperatorIsEqualTo value:identifier];
+    query.limit = 1;
+    return [self.layerClient executeQuery:query error:nil].firstObject;
+}
+
+- (LYRConversation *)existingConversationForParticipants:(NSSet *)participants
+{
+    LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRConversation class]];
+    query.predicate = [LYRPredicate predicateWithProperty:@"participants" predicateOperator:LYRPredicateOperatorIsEqualTo value:participants];
+    query.limit = 1;
+    return [self.layerClient executeQuery:query error:nil].firstObject;
 }
 
 #pragma mark - Notification Handlers
