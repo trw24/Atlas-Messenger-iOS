c555469c60b78ee9e47d8627683c6d0b7a6cbe1e
Daniel Maness <daniel@layer.com>
2016-11-11 11:24:42 -0800

    Created DI-branded version of Atlas Messenger that works with InstaStart Identity Provider

---
+++ Code/Models/UserCredentials.h
@@ -0,0 +1,13 @@
+//
+//  UserCredentials.h
+//  DI Messenger
+//
+//  Created by Daniel Maness on 11/10/16.
+//  Copyright © 2016 Layer, Inc. All rights reserved.
+//
+
+#ifndef UserCredentials_h
+#define UserCredentials_h
+
+
+#endif /* UserCredentials_h */
---
+++ Code/Models/UserCredentials.m
@@ -0,0 +1,9 @@
+//
+//  UserCredentials.m
+//  DI Messenger
+//
+//  Created by Daniel Maness on 11/10/16.
+//  Copyright © 2016 Layer, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
