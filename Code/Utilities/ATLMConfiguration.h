9507bfc41b2fae494fc8b6fbfc4b3e38c1e61996
Klemen Verdnik <klemen.verdnik@gmail.com>
2017-02-15 18:21:57 -0800

    Added documentation blocks to ATLMConfiguration header.

--- Code/Utilities/ATLMConfiguration.h
+++ Code/Utilities/ATLMConfiguration.h
@@ -21,13 +21,47 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+
+/**
+ @abstract The purpose of this class is to deserialize JSON values from a
+   configuration file into @c appID and @c identityProviderURL properties.
+   The input configuration file (@c fileURL) should be a text encoded JSON
+   structure containing two values keyed by "app_id" and "identity_provider_url".
+   The JSON structure should have no root key elements.
+ @discussion See the following JSON structure example
+ @code
+ {
+   "app_id": "layer:///apps/staging/00000000-0000-0000-0000-000000000000",
+   "identity_provider_url" : "https://foo.bar"
+ }
+ @endcode
+ */
 @interface ATLMConfiguration : NSObject
 
+/**
+ @abstract The deserialized value of the `appID` found in the input
+   JSON configuration file.
+ @see -initWithFileURL:
+ */
 @property (nonatomic, readonly) NSURL *appID;
+
+/**
+ @abstract The deserialized value of the `identityProviderURL` found in the
+   input JSON configuration file.
+ @see -initWithFileURL:
+ */
 @property (nonatomic, readonly) NSURL *identityProviderURL;
 
+/**
+ @abstract Initializes an `ATLMConfiguration` instance and loads the values
+   from the input JSON file (`fileURL`) into the instance properties.
+ @param fileURL A file path in a form of an `NSURL` instance pointing to 
+   a configuration text file, containing a JSON structure.
+ @return Returns an intialized instance of `ATLMConfiguration`.
+ */
 - (instancetype)initWithFileURL:(nonnull NSURL *)fileURL NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE; 
 
 @end
+
 NS_ASSUME_NONNULL_END
